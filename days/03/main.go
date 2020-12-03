91138307fcd64cabc8c1c275ea4a3f2f7b0d0565
Matias Lindgren <matias.lindgren@iki.fi>
2020-12-03 08:39:17 +0200

    cleanup

--- days/03/main.go
+++ days/03/main.go
@@ -5,28 +5,27 @@ import (
 	"fmt"
 )
 
-func numTrees(lines []string, dx, dy int) (int) {
-	a := 0
+func countTrees(lines []string, dx, dy int) (int) {
+	numTrees := 0
 	for x, y := 0, 0; y < len(lines); x, y = (x+dx)%len(lines[0]), y+dy {
 		if lines[y][x] == '#' {
-			a++
+			numTrees++
 		}
 	}
-	return a
+	return numTrees
 }
 
 func search(lines []string) (int, int) {
-	a := numTrees(lines[:], 3, 1)
+	a := countTrees(lines[:], 3, 1)
 	b := a
 	delta := [][]int{{1, 1}, {5, 1}, {7, 1}, {1, 2}}
 	for _, pair := range delta {
-		b *= numTrees(lines[:], pair[0], pair[1])
+		b *= countTrees(lines[:], pair[0], pair[1])
 	}
 	return a, b
 }
 
 func main() {
-	input := util.SlurpStdinLines()
-	a, b := search(input)
+	a, b := search(util.SlurpStdinLines())
 	fmt.Println(a, b)
 }
