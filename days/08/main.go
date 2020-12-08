c4edb76a803b006cd8bc96dc94e2cc9e6c5b6199
Matias Lindgren <matias.lindgren@iki.fi>
2020-12-08 09:45:45 +0200

    simplify

--- days/08/main.go
+++ days/08/main.go
@@ -11,12 +11,10 @@ func parseInstruction(line string) (string, int) {
 	return parts[0], util.ParseInt(parts[1])
 }
 
-func run(lines []string, mutateLine int) (int, bool) {
-	acc := 0
-	visited := map[int]bool{}
-	i := 0
-	for ; !visited[i] && i < len(lines); i++ {
-		visited[i] = true
+func tryRun(lines []string, mutateLine int) (int, bool) {
+	acc, i := 0, 0
+	for seen := map[int]bool{}; !seen[i] && i < len(lines); i++ {
+		seen[i] = true
 		ins, num := parseInstruction(lines[i])
 		if i == mutateLine {
 			switch ins {
@@ -33,23 +31,19 @@ func run(lines []string, mutateLine int) (int, bool) {
 			i += num - 1
 		}
 	}
-	return acc, i == len(lines)
+	return acc, i < len(lines)
 }
 
 func search(lines []string) (int, int) {
-	a, _ := run(lines, -1)
+	a, _ := tryRun(lines, -1)
 	b := 0
 	for mutateLine, line := range lines {
 		ins, _ := parseInstruction(line)
-		switch ins {
-		case "nop":
-		case "jmp":
-		default:
-			continue
-		}
-		if acc, visitedAll := run(lines, mutateLine); visitedAll {
-			b = acc
-			break
+		if ins == "nop" || ins == "jmp" {
+			if acc, earlyExit := tryRun(lines, mutateLine); !earlyExit {
+				b = acc
+				break
+			}
 		}
 	}
 	return a, b
