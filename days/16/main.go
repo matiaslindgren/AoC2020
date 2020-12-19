a19a984ea9a550cda8b2c4d136fd4bd32814f56c
Matias Lindgren <matias.lindgren@iki.fi>
2020-12-19 21:22:57 +0200

    simplify 16

--- days/16/main.go
+++ days/16/main.go
@@ -11,7 +11,7 @@ var Split = strings.Split
 
 type Rule struct {
 	Name string
-	R [4]int
+	aMin, aMax, bMin, bMax int
 }
 
 func parseRule(r string) Rule {
@@ -19,7 +19,7 @@ func parseRule(r string) Rule {
 	name, parts := parts[0], Split(parts[1], " ")
 	a, b := Split(parts[0], "-"), Split(parts[2], "-")
 	f := util.ParseInt
-	return Rule{name, [...]int{f(a[0]), f(a[1]), f(b[0]), f(b[1])}}
+	return Rule{name, f(a[0]), f(a[1]), f(b[0]), f(b[1])}
 }
 
 func parseSections(input string) ([]Rule, string, []string) {
@@ -33,11 +33,15 @@ func parseSections(input string) ([]Rule, string, []string) {
 	return rules, ticket, tickets
 }
 
-func (r Rule) valid(x int) bool {
-	return (r.R[0] <= x && x <= r.R[1]) || (r.R[2] <= x && x <= r.R[3])
+func parseTicket(t string) []int {
+	return util.ParseIntArray(Split(t, ","))
 }
 
-func allValid(rules []Rule, x int) (bool) {
+func (r Rule) valid(x int) bool {
+	return (r.aMin <= x && x <= r.aMax) || (r.bMin <= x && x <= r.bMax)
+}
+
+func hasValidRule(rules []Rule, x int) bool {
 	for _, rule := range rules {
 		if rule.valid(x) {
 			return true
@@ -46,16 +50,15 @@ func allValid(rules []Rule, x int) (bool) {
 	return false
 }
 
-func numtrue(m map[int]bool) (int, int) {
-	n := 0
-	i := 0
-	for k, v := range m {
-		if v {
-			n++
-			i = k
+func makeFilterTable(a, b int) [][]bool {
+	T := make([][]bool, a)
+	for i := 0; i < a; i++ {
+		T[i] = make([]bool, b)
+		for j := range T[i] {
+			T[i][j] = true
 		}
 	}
-	return i, n
+	return T
 }
 
 func search(input string) (int, int) {
@@ -63,60 +66,51 @@ func search(input string) (int, int) {
 
 	a := 0
 	validTickets := [][]int{}
-	for _, t := range tickets {
-		v := util.ParseIntArray(Split(t, ","))
+	for _, strTicket := range tickets {
+		ticket := parseTicket(strTicket)
 		ok := true
-		for _, x := range v {
-			if !allValid(rules, x) {
+		for _, x := range ticket {
+			if !hasValidRule(rules, x) {
 				a += x
 				ok = false
 				break
 			}
 		}
 		if ok {
-			validTickets = append(validTickets, v)
+			validTickets = append(validTickets, ticket)
 		}
 	}
 
-	candidates := map[int]map[int]bool{}
-	for i := 0; i < len(validTickets[0]); i++ {
-		candidates[i] = map[int]bool{}
+	ticketLen := len(validTickets[0])
+	pos2validRules := makeFilterTable(ticketLen, len(rules))
+	for pos, valid := range pos2validRules {
+		for r, rule := range rules {
+			for _, ticket := range validTickets {
+				valid[r] = valid[r] && rule.valid(ticket[pos])
+			}
+		}
 	}
-	for i, rule := range rules {
-		for col := 0; col < len(validTickets[0]); col++ {
-			for _, t := range validTickets {
-				x := t[col]
-				if rule.valid(x) {
-					candidates[col][i] = true
-				} else {
-					candidates[col][i] = false
-					break
+
+	pos2rule := make([]Rule, ticketLen)
+	for ruleTaken := map[int]bool{}; len(ruleTaken) < len(rules); {
+		for pos, validRules := range pos2validRules {
+			numFree, prev := 0, 0
+			for r, valid := range validRules {
+				if valid && !ruleTaken[r] {
+					numFree++
+					prev = r
 				}
 			}
-		}
-	}
-
-	col2rule := map[int]Rule{}
-	for i := 0; len(col2rule) < len(validTickets[0]); {
-		for col, cand := range candidates {
-			j, numt := numtrue(cand)
-			if numt == 1 {
-				col2rule[col] = rules[j]
-				i = j
-				break
+			if numFree == 1 {
+				pos2rule[pos] = rules[prev]
+				ruleTaken[prev] = true
 			}
-			if numt == 0 {
-				delete(candidates, col)
-			}
-		}
-		for _, cand := range candidates {
-			delete(cand, i)
 		}
 	}
 
 	b := 1
-	for col, x := range util.ParseIntArray(Split(myTicket, ",")) {
-		if strings.Contains(col2rule[col].Name, "departure") {
+	for pos, x := range util.ParseIntArray(Split(myTicket, ",")) {
+		if strings.Contains(pos2rule[pos].Name, "departure") {
 			b *= x
 		}
 	}
