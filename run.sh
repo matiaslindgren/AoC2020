992025bf8f67fd222afcbf2d464ba87824a495f3
Matias Lindgren <matias.lindgren@iki.fi>
2021-02-13 13:20:53 +0200

    fix unsynced time output

--- run.sh
+++ run.sh
@@ -37,8 +37,8 @@ function run_day {
 
 if [ $# -eq 0 ]; then
 	for day in $(find bin -regex 'bin/[0-2][0-9]$' | sort); do
-		time run_day $(basename $day)
+		time run_day $(basename $day) 2>&1
 	done
 else
-	time run_day $1
+	time run_day $1 2>&1
 fi
