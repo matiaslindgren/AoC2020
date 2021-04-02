54bd36347a8d04b5bbd739481c626bb77abfce03
Matias Lindgren <matias.lindgren@iki.fi>
2021-04-02 16:04:53 +0300

    solve day 20

--- days/20/main.go
+++ days/20/main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/matiaslindgren/AoC2020/util"
 )
+
+const (
+	UP = 0
+	RIGHT = 1
+	DOWN = 2
+	LEFT = 3
+)
+var monsterPattern = [3]string{
+	"..................#..",
+	"#....##....##....###.",
+	".#..#..#..#..#..#....",
+}
+var monsterRegexp = [3]*regexp.Regexp{
+	regexp.MustCompile(monsterPattern[0]),
+	regexp.MustCompile(monsterPattern[1]),
+	regexp.MustCompile(monsterPattern[2]),
+}
 
 func parseTile(section string) Tile {
 	t := Tile{}
@@ -13,15 +31,15 @@ func parseTile(section string) Tile {
 	lines := strings.Split(section, "\n")
 	t.id = lines[0][5:9]
 
-	t.borders[0] = lines[1]
+	t.borders[UP] = lines[1]
 	left, right := []byte{}, []byte{}
 	for _, line := range lines[1:] {
 		left = append(left, line[0])
 		right = append(right, line[len(line)-1])
 	}
-	t.borders[1] = string(right)
-	t.borders[2] = reverse(lines[len(lines)-1])
-	t.borders[3] = reverse(string(left))
+	t.borders[RIGHT] = string(right)
+	t.borders[DOWN] = reverse(lines[len(lines)-1])
+	t.borders[LEFT] = reverse(string(left))
 
 	for _, line := range lines[2:len(lines)-1] {
 		t.img = append(t.img, line[1:len(line)-1])
@@ -45,12 +63,7 @@ type Tile struct {
 	borders [4]string
 }
 
-type Match struct {
-	tile1, tile2 Tile
-	border1, border2 int
-}
-
-type Tiles map[string]Tile
+type TileMap map[string]Tile
 
 func rotate(t1 Tile) Tile {
 	n := len(t1.img)
@@ -67,8 +80,8 @@ func rotate(t1 Tile) Tile {
 		t2.img[y] = string(row)
 	}
 
-	for i, b := range t1.borders {
-		t2.borders[(i+1)%4] = b
+	for side, b := range t1.borders {
+		t2.borders[(side+1)%4] = b
 	}
 
 	return t2
@@ -85,86 +98,199 @@ func flip(t1 Tile) Tile {
 		t2.img[n-y-1] = row
 	}
 
-	t2.borders[0] = reverse(t1.borders[2])
-	t2.borders[1] = reverse(t1.borders[1])
-	t2.borders[2] = reverse(t1.borders[0])
-	t2.borders[3] = reverse(t1.borders[3])
+	for i2, i1 := range []int{DOWN, RIGHT, UP, LEFT} {
+		t2.borders[i2] = reverse(t1.borders[i1])
+	}
 
 	return t2
 }
 
-// func (t Tile) print() {
-// 	fmt.Println(t.id)
-// 	for i, b := range t.borders {
-// 		fmt.Println(i, b)
-// 	}
-// 	fmt.Println()
-// 	for i, line := range t.img {
-// 		fmt.Println(i, line)
-// 	}
-// 	fmt.Println()
-// }
+func (in *TileMap) copy() *TileMap {
+	out := TileMap{}
+	for id, t := range *in {
+		out[id] = t
+	}
+	return &out
+}
 
-func matchBorders(t1, t2 Tile) (int, int, bool) {
-	for i1, b1 := range t1.borders {
-		for i2, b2 := range t2.borders {
-			if b1 == b2 || b1 == reverse(b2) {
-				return i1, i2, true
+func (t1 Tile) findMatchOrientation(t2 Tile, side int) (Tile, bool) {
+	for flips := 0; flips < 2; flips++ {
+		for rotations := 0; rotations < 4; rotations++ {
+			if t1.borders[side] == reverse(t2.borders[(side+2)%4]) {
+				return t2, true
+			}
+			t2 = rotate(t2)
+		}
+		t2 = flip(t2)
+	}
+	return t2, false
+}
+
+type Grid map[int]map[int]Tile
+
+func (g *Grid) set(x, y int, t Tile) {
+	if col, exists := (*g)[x]; exists {
+		col[y] = t
+	} else {
+		(*g)[x] = map[int]Tile{y: t}
+	}
+}
+
+func (g *Grid) cornerIndexes() (int, int, int, int) {
+	minX := 1<<63-1
+	maxX := -minX
+	maxY := maxX
+	minY := minX
+	for x, col := range *g {
+		maxX = util.Max(maxX, x)
+		minX = util.Min(minX, x)
+		for y := range col {
+			maxY = util.Max(maxY, y)
+			minY = util.Min(minY, y)
+		}
+	}
+	return minX, maxX, minY, maxY
+}
+
+func (g *Grid) count() int {
+	minX, maxX, minY, maxY := g.cornerIndexes()
+	n := 0
+	for y := maxY; y >= minY; y-- {
+		for x := minX; x <= maxX; x++ {
+			if len((*g)[x][y].id) > 0 {
+				n++
 			}
 		}
 	}
-	return -1, -1, false
+	return n
 }
 
-func findCorners(tiles Tiles) [4]string {
-	tile2matchCount := map[string]int{}
-	for _, t1 := range tiles {
-		for _, t2 := range tiles {
-			if t1.id == t2.id {
+func (g *Grid) corners() [4]Tile {
+	minX, maxX, minY, maxY := g.cornerIndexes()
+	return [4]Tile{
+		(*g)[minX][minY],
+		(*g)[minX][maxY],
+		(*g)[maxX][minY],
+		(*g)[maxX][maxY],
+	}
+}
+
+func posDelta(side int) (int, int) {
+	switch side {
+	case UP:
+		return 0, 1
+	case RIGHT:
+		return 1, 0
+	case DOWN:
+		return 0, -1
+	case LEFT:
+		return -1, 0
+	default:
+		panic(fmt.Sprint("unknown side ", side))
+	}
+}
+
+func buildGrid(t1 Tile, x, y int, tiles *TileMap, frozen *map[string]bool, grid *Grid) *Grid {
+	(*frozen)[t1.id] = true
+	grid.set(x, y, t1)
+	for side := 0; side < 4; side++ {
+		for _, t := range *tiles {
+			if (*frozen)[t.id] {
 				continue
 			}
-			if _, _, ok := matchBorders(t1, t2); ok {
-				tile2matchCount[t1.id]++
+			if t2, ok := t1.findMatchOrientation(t, side); ok {
+				dx, dy := posDelta(side)
+				grid = buildGrid(t2, x+dx, y+dy, tiles, frozen, grid)
 			}
 		}
 	}
-	i, corners := 0, [4]string{}
-	for id, c := range tile2matchCount {
-		if c == 2 {
-			corners[i] = id
-			i++
-		}
-	}
-	if i != 4 {
-		panic(fmt.Sprintf("wrong amount of corners %d != 4", i))
-	}
-	return corners
+	return grid
 }
 
-func buildGrid(tiles Tiles, start Tile) ([]string, bool) {
+func (g *Grid) buildImage(tileWidth int) []string {
+	img := []string{}
+	minX, maxX, minY, maxY := g.cornerIndexes()
+	for y := maxY; y >= minY; y-- {
+		for yTile := 0; yTile < tileWidth; yTile++ {
+			row := []string{}
+			for x := minX; x <= maxX; x++ {
+				if t, exists := (*g)[x][y]; exists {
+					row = append(row, t.img[yTile])
+				}
+			}
+			if len(row) > 0 {
+				img = append(img, strings.Join(row, ""))
+			}
+		}
+	}
+	return img
+}
+
+func hasMonster(img []string, x, y, w int) bool {
+	for i, re := range monsterRegexp {
+		if !re.MatchString(img[y+i][x:x+w]) {
+			return false
+		}
+	}
+	return true
+}
+
+func countMonsters(img []string) int {
+	n := 0
+	h, w := len(monsterPattern), len(monsterPattern[0])
+	for y := 0; y < len(img)-h; y++ {
+		for x := 0; x < len(img[0])-w; x++ {
+			if hasMonster(img, x, y, w) {
+				n++
+				x += w-1
+			}
+		}
+	}
+	return n
+}
+
+func countRoughness(img []string) int {
+	if numMonsters := countMonsters(img); numMonsters > 0 {
+		numAll := strings.Count(strings.Join(img, ""), "#")
+		numMonster := strings.Count(strings.Join(monsterPattern[:], ""), "#")
+		return numAll - numMonsters * numMonster
+	}
+	return 0
+}
+
+func findMonsterGrid(tiles TileMap) (*Grid, []string) {
+	for _, t := range tiles {
+		for flips := 0; flips < 2; flips++ {
+			for rotations := 0; rotations < 4; rotations++ {
+				grid := buildGrid(t, 0, 0, tiles.copy(), &map[string]bool{}, &Grid{})
+				if grid.count() == len(tiles) {
+					img := grid.buildImage(len(t.img))
+					if n := countMonsters(img); n > 0 {
+						return grid, img
+					}
+				}
+				t = rotate(t)
+			}
+			t = flip(t)
+		}
+	}
+	return nil, nil
 }
 
 func search(input []string) (int, int) {
-	originalTiles := Tiles{}
+	tiles := TileMap{}
 	for _, section := range input {
 		t := parseTile(section)
-		originalTiles[t.id] = t
+		tiles[t.id] = t
 	}
 
-	corners := findCorners(originalTiles)
+	grid, img := findMonsterGrid(tiles)
+
 	a := 1
-	for _, id := range corners {
-		a *= util.ParseInt(id)
+	for _, c := range grid.corners() {
+		a *= util.ParseInt(c.id)
 	}
-
-	for _, id := range corners {
-		grid, ok := buildGrid(originalTiles, originalTiles[id])
-		if ok {
-			fmt.Println(grid)
-		}
-	}
-
-	b := 0
+	b := countRoughness(img)
 	return a, b
 }
 
@@ -172,8 +298,4 @@ func main() {
 	input := util.SlurpStdinSections()
 	a, b := search(input)
 	fmt.Println(a, b)
-	fmt.Println(
-`1951 2311 3079
-2729 1427 2473
-2971 1489 1171`)
 }
